642aad0064bf5c7bf89bcb77087cc9c81af8da87
frederic <frederic@askarel.be>
2015-04-07 00:41:45 +0200

    Added CGI script to echo back your public IP address

---
+++ getip.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# getip.sh - Tiny CGI script to print out your public IP address
+#
+#    Copyright (C) 2015  Frederic Pasteleurs <frederic@askarel.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# This is going to be terse
+
+printf 'Content-type: text/plain\n\n'
+
+echo "$REMOTE_ADDR"
